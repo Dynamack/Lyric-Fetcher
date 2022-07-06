15ff54d61b2d76c6b091e91256b95c181e84113c
Odhran <91205552+Dynamack@users.noreply.github.com>
2022-07-06 18:14:40 +0100

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Config for LyricFetcher.py
-
-client_id = 'OSntcfazV-UI1ZNm3996bcYqvyPnbAPVdRPW4bGRJR9iALhph--rCCDKfGGGheXJ'
-client_secret = 'S5xqU2GOXvegJn4y1YQ5stWdK7543zlIOTtzXytRAOa7ObSeeIrvnJNEFf87ecwvR9HuugDdjJR7gBC3IUySbA'
-# some other info blah blah
